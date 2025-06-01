2ecfd03b5e756061195c1ec67192db39d4b9ea24
Sam Hopkins <cakepielore@gmail.com>
2025-05-31 23:16:00 -0400

    Fixing formatting

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,19 @@
 from http.client import HTTPException
-from fastapi import FastAPI, Query, APIRouter
-from starlette.responses import HTMLResponse
-from starlette.templating import Jinja2Templates
-
+from fastapi import FastAPI, Query, APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from app.mal_api import search_anime_by_title, get_client_id
 router = APIRouter()
 CLIENT_ID = get_client_id()
-
 templates = Jinja2Templates(directory="app/templates")
 @router.get("/search-ui", summary="Search anime by title", response_description="List of anime matching the search title", response_class=HTMLResponse)
-async def search_anime_ui(title: str = Query(..., min_length=1, description="Title of the anime to search")):
+async def search_anime_ui(
+    request: Request,
+    title: str = Query(..., min_length=1, description="Title of the anime to search")
+):
     if not CLIENT_ID:
         raise Exception("CLIENT_ID is not set")
+
     results = search_anime_by_title(title, CLIENT_ID)
 
     if not results or "data" not in results:
@@ -27,4 +29,7 @@ async def search_anime_ui(title: str = Query(..., min_length=1, description="Tit
             "image": node.get("main_picture", {}).get("medium", "N/A")
         })
 
-    return {"results": formatted}
+    return templates.TemplateResponse("results.html", {
+        "request": request,
+        "results": formatted
+    })
